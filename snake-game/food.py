0cf1f193d71bac67cf2fd49ccd4eae5b09bff76a
azra irem <143757501+wowazraa@users.noreply.github.com>
2025-07-14 13:22:47 +0300

    Create food.py

---
+++ snake-game/food.py
@@ -0,0 +1,17 @@
+from turtle import *
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        x_cor = random.randint(-280, 280)
+        y_cor = random.randint(-280, 280)
+        self.goto(x_cor, y_cor)
