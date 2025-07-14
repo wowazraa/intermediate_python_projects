1abdf1836a2e893bf634c6bbf24b157972be770c
azra irem <143757501+wowazraa@users.noreply.github.com>
2025-07-14 13:21:50 +0300

    Create main.py

---
+++ snake-game/main.py
@@ -0,0 +1,52 @@
+from turtle import *
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import random
+import time
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+is_over = False
+screen.update()
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+while not is_over:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        scoreboard.score_up()
+        snake.extend()
+        food.refresh()
+
+    # detect collision with wall
+    if not 280 >= snake.head.xcor() >= -280 or not 280 >= snake.head.ycor() >= -280:
+        is_over = True
+        scoreboard.game_over()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_over = True
+            scoreboard.game_over()
+
+    # if the head collides with any segments in the tail
+        # trigger game over
+
+screen.exitonclick()
