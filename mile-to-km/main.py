72c3bfb0fc9e8b00cf3e8d96aad685910d7453bd
azra irem <143757501+wowazraa@users.noreply.github.com>
2025-07-27 11:57:07 +0300

    Create main.py

---
+++ mile-to-km/main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+def convert():
+    miles = float(mile.get())
+    km["text"] = miles * 1.60934
+
+window = Tk()
+window.minsize(width=200, height=100)
+window.config(padx=10, pady=10)
+window.title("Mile to Km Converter")
+
+label_equal = Label(text="is equal to")
+label_equal.grid(column=0, row=1)
+
+label_mile = Label(text="Miles")
+label_mile.grid(column=2, row=0)
+
+label_km = Label(text="Km")
+label_km.grid(column=2, row=1)
+
+km = Label(text=0)
+km.grid(column=1, row=1)
+
+mile = Entry()
+mile.insert(END, text="0")
+mile.grid(column=1, row=0)
+
+button = Button(text="Calculate", command=convert)
+button.grid(column=1, row=2)
+
+window.mainloop()
