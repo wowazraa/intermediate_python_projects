6d67e30f82ad462906d768079756a35ee2da1c0a
azra irem <143757501+wowazraa@users.noreply.github.com>
2025-07-31 16:42:53 +0300

    Update main.py

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -29,20 +30,67 @@ def save():
     email = email_entry.get()
     passw = pass_entry.get()
 
+    if email == "" or email == "default@gmail.com":
+        messagebox.showinfo(title="Oops.", message="Please enter your email.")
+        return
+
     if web == "" or email == "" or passw == "":
         messagebox.showinfo(title="Oops", message="Please dont' leave any fields empty.")
     else:
         is_ok = messagebox.askokcancel(title=web, message=f"These are the details entered: "
                                                   f"\nEmail: {email} \nPassword: {passw} \n"
                                                   f"Is it okay to save?")
+
+        new_data = {
+            web: {
+                "email": email,
+                "password": passw,
+            }
+        }
+
         if is_ok:
-            with open("data.txt", "a") as data:
-                data.write(f"{web} | {email} | {passw}\n")
+            try:
+                with open("data.json", "r") as data_file:
+                    # reading old data
+                    data = json.load(data_file)
+
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                # updating old data with new data data
+                data.update(new_data)
+
+                with open("data.json", "w") as data_file:
+                    # saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
                 web_entry.delete(0, END)
                 pass_entry.delete(0, END)
+
+def find_password():
+    user_text = web_entry.get()
+
+    try:
+        with open("data.json") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops.", message="No Data File Found")
+    else:
+            if user_text in data:
+                email = data[user_text]["email"]
+                password = data[user_text]["password"]
+
+                messagebox.showinfo(title="", message=f"Email: {email}\nPassword: {password}")
+            else:
+                messagebox.showinfo(title="Oops.", message="No details for the website exists.")
+
+            print(json.dumps(data, indent=4))  # for debug
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
+window.minsize(width=500, height=400)
 window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
@@ -61,10 +109,11 @@ text_pass.grid(row=3, column=0)
 
 web_entry = Entry()
 web_entry.focus()
-web_entry.grid(row=1, column=1, columnspan=2, sticky="ew")
+web_entry.grid(row=1, column=1, columnspan=2, sticky = "ew")
 
 email_entry = Entry()
 email_entry.insert(0, "default@gmail.com")
+
 email_entry.grid(row=2, column=1, columnspan=2, sticky="ew")
 
 pass_entry = Entry()
@@ -73,7 +122,11 @@ pass_entry.grid(row=3, column=1, sticky="ew")
 generate = Button(text="Generate Password", command=generate_password)
 generate.grid(row=3, column=2, sticky="ew")
 
+
 add = Button(text="Add", command=save)
 add.grid(row=4, column=1, columnspan=2, sticky="ew")
+
+search = Button(text="Search", command=find_password)
+search.grid(row=1, column=2, sticky="ew")
 
 window.mainloop()
